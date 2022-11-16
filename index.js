fbfe816c9b44785ec8c5726124d84a6a1b5c6c86
Jishnu <jishnunbhattathiri@gmail.com>
2022-11-16 08:03:47 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+
+
+const todo_example1 = async (req, res) => {
+
+  // health check
+  if (req.params["health"] === "health") {
+    res.write(JSON.stringify({success: true, msg: "Health check success"}))
+    res.end()
+  }
+
+  // Add your code here
+  res.write(JSON.stringify({success: true, msg: `Hello todo_example1`}))
+  res.end()
+  
+}
+
+export default todo_example1
